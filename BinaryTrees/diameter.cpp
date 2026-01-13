ad6ad34ce34f15e97e7ff59e3cdabd3653315e73
charvi-web <charvisingh077@gmail.com>
2026-01-13 18:27:40 +0530

    height ,diameter, check for balanced BT

---
+++ BinaryTrees/diameter.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int height(TreeNode* root, int &dia) {   // pass by reference
+        if (root == nullptr) return 0;
+
+        int lh = height(root->left, dia);
+        int rh = height(root->right, dia);
+
+        dia = max(dia, lh + rh);  // update diameter
+
+        return 1 + max(lh, rh);
+    }
+
+    int diameter(TreeNode* root) {
+        int dia = 0;
+        height(root, dia);
+        return dia;
+    }
+};

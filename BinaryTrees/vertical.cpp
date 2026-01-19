8bce7d23c35044677342cecf23236f4ed10a8b56
charvi-web <charvisingh077@gmail.com>
2026-01-19 23:17:58 +0530

    Vertical

---
+++ BinaryTrees/vertical.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;class Solution {
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
+        map<int, map<int, multiset<int>>> ds;
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            TreeNode* node = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
+
+            ds[x][y].insert(node->val);
+
+            if (node->left)
+                q.push({node->left, {x - 1, y + 1}});
+            if (node->right)
+                q.push({node->right, {x + 1, y + 1}});
+        }
+
+        vector<vector<int>> ans;
+        for (auto& ito : ds) {
+            vector<int> col;
+            for (auto& iti : ito.second) {
+                col.insert(col.end(), iti.second.begin(), iti.second.end());
+            }
+            ans.push_back(col);
+        }
+
+        return ans;
+    }
+};

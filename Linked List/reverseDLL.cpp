f22f634f34bb8039e7d2b35b17cb10e957e33285
charvi-web <charvisingh077@gmail.com>
2026-01-29 18:21:51 +0530

    Reverse DLL

---
+++ Linked List/reverseDLL.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class ListNode {
+public:
+    int data;
+    ListNode* prev;
+    ListNode* next;
+
+    ListNode(int val) : data(val), prev(nullptr), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    ListNode* reverseDLL(ListNode* head) {
+        // Your code goes here
+        if (head==nullptr || head->next==nullptr) return head;
+        ListNode* temp = head;
+        ListNode* last = nullptr;
+        while (temp)
+        {
+            last = temp->prev;
+            temp->prev=temp->next;
+            temp->next=last;
+            temp=temp->prev;
+        }
+        return last->prev;
+    }
+};

b2f2b5d4033af4073ba6115c1f08b3abb32cfcbe
charvi-web <charvisingh077@gmail.com>
2026-01-30 17:39:18 +0530

    sorting LL 0,1,2

---
+++ Linked List/sort012.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Definition for singly-linked list.
+ struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head==nullptr || head->next==nullptr) return head;
+        ListNode* zeroHead = new ListNode(-1);
+        ListNode* oneHead = new ListNode(-1);
+        ListNode* twoHead = new ListNode(-1);
+        ListNode* zero = zeroHead;
+        ListNode* one = oneHead;
+        ListNode* two = twoHead;
+
+        ListNode* temp = head;
+        while(temp)
+        {
+            if (temp->val==0) 
+            {
+                zero->next=temp;
+                zero=zero->next;
+            }
+            else if (temp->val==1)
+            {
+                one->next=temp;
+                one=one->next;
+            }
+            else 
+            {
+                two->next=temp;
+                two=two->next;
+            }
+
+            temp=temp->next;
+        }
+        two->next=nullptr;
+        zero->next = oneHead->next ? oneHead->next : twoHead->next;
+        one->next = twoHead->next;
+    return zeroHead->next;
+    }
+};

29290740f4bcca7819670c5d0ba97ee57ee42c91
charvi-web <charvisingh077@gmail.com>
2026-02-04 23:12:51 +0530

    changes

---
+++ Array/leaders.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        vector<int>ans;
+      for (int i=n-1;i>=0;i--)
+      {
+        if (nums[i]>maxi)
+        {
+            ans.push_back(nums[i]);
+        }
+        maxi = max(maxi,nums[i]);
+      }
+      reverse(ans.begin(),ans.end());
+      return ans;
+    }
+};

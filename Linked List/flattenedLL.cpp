9d1346b1b2e29a6ef63a6476af24e7017d47dfb4
charvi-web <charvisingh077@gmail.com>
2026-02-27 19:48:48 +0530

    Flatten LL

---
+++ Linked List/flattenedLL.cpp
@@ -0,0 +1,71 @@
+
+//Definition of special linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *child;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        child = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        child = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode* next2)
+    {
+        val = data1;
+        next = next1;
+        child = next2;
+    }
+};
+
+ListNode* merge(ListNode* list1,ListNode* list2)
+{
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+    ListNode* t1 = list1;
+    ListNode* t2 = list2;
+    while (t1 && t2)
+    {
+        if (t1->val < t2->val)
+        {
+            temp->child=t1;
+            
+            t1=t1->child;
+
+        }
+        else 
+        {
+            temp->child=t2;
+           
+            t2=t2->child;
+
+        }
+         temp=temp->child;
+            temp->next=NULL;
+    }
+    if (t1) {temp->child=t1;}
+    else {temp->child=t2;}
+    
+return dummy->child;
+}
+class Solution {
+public:
+    ListNode* flattenLinkedList(ListNode* &head) {
+        if (head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        head->next = flattenLinkedList(head->next);
+        head =  merge(head,head->next);
+        head->next= NULL;
+        return head;
+
+    }
+};

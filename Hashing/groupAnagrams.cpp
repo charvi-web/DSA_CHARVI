ebc9beee9553e16f8998b077ed43ef4d16f6b5b0
charvi-web <charvisingh077@gmail.com>
2026-03-02 13:45:28 +0530

    Group Anagrams

---
+++ Hashing/groupAnagrams.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>m;
+
+        for (string s : strs)
+        {
+            string temp = s;
+            sort(temp.begin(),temp.end());
+            m[temp].push_back(s);
+        }
+        vector<vector<string>>ans;
+        for (auto it : m)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};

8e1fdab4341d5b2d5fabbe888fcde5856721c490
charvi-web <charvisingh077@gmail.com>
2026-03-06 16:54:17 +0530

    Previous smaller element

---
+++ Stacks Queues/prevSmallerElement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextSmallerElements(vector<int>& arr) {
+        // Your code goes here
+        stack<int>st;
+        int n = arr.size();
+        vector<int>res(n,-1);
+        for (int i=0;i<n;i++)
+        {
+            while(!st.empty() && st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if (!st.empty()) res[i] = st.top();
+            st.push(arr[i]);
+        }
+    return res;
+    }
+};

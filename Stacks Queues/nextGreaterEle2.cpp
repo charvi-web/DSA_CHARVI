88ead643872d18b03770a1c1a0b6f114989d9bc0
charvi-web <charvisingh077@gmail.com>
2026-03-06 16:34:05 +0530

    next greater element 2

---
+++ Stacks Queues/nextGreaterEle2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        int n = nums.size();
+        vector<int>res(n,-1);
+        
+        for (int i=2*n-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums[i%n])
+            st.pop();
+            if (!st.empty()) res[i%n]=st.top();
+            st.push(nums[i%n]);
+
+        }
+        return res;
+    }
+};

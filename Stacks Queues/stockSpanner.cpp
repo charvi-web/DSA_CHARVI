c8cacd8f09ca9bd01f9c37433f620c81c02f78e9
charvi-web <charvisingh077@gmail.com>
2026-03-09 23:28:15 +0530

    Stock Spanner

---
+++ Stacks Queues/stockSpanner.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class StockSpanner {
+
+    public:
+    stack<pair<int,int>>st;
+        int ind;
+public:
+
+    StockSpanner() {
+        ind = -1;
+    }
+    
+    int next(int price) {
+        
+        ind = ind+1;
+        while (!st.empty() && st.top().first<=price)
+        st.pop();
+        int ans;
+        if (st.empty()) ans = ind+1;
+        else ans = ind - st.top().second;
+        st.push({price,ind});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */

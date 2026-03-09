e3c47970361daadb3c412bdb9f70e8e04fea342a
charvi-web <charvisingh077@gmail.com>
2026-03-10 00:16:17 +0530

    Sliding window maximum

--- Stacks Queues/slidingWindowMaxi.cpp
+++ Stacks Queues/slidingWindowMaxi.cpp
@@ -10,7 +10,7 @@ public:
         {
             if (!dq.empty() && dq.front()<=i-k) dq.pop_front();
             while (!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
-            dq.push(i);
+            dq.push_back(i);
             if (i>=k-1)
             {
                 ans.push_back(nums[dq.front()]);

92db04975fa917ebac605cf9022a52026779d925
charvi-web <charvisingh077@gmail.com>
2026-03-25 01:06:00 +0530

    Maximum product subarray

--- PREFIXSUFFIX/maximumProductsubarray.cpp
+++ PREFIXSUFFIX/maximumProductsubarray.cpp
@@ -4,27 +4,42 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int maxP=INT_MIN;
-        int n = nums.size();
-        vector<int>P(n,0);
-        long long prefix = 1;
-        for(int i=0;i<n;i++)
-        {
-            prefix=prefix*nums[i];
-            P[i]=prefix;
-        }
+        // int n = nums.size();
+        // vector<long long>P(n,0);
+        // long long prefix = 1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     prefix=prefix*nums[i];
+        //     P[i]=prefix;
+        // }
 
+        // for (int i=0;i<n;i++)
+        // {
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         long long a;
+        //         if (i==0) a=P[j];
+        //         else if (P[i-1]!=0) a = P[j]/P[i-1];
+        //         else continue;
+        //     maxP = max(maxP,(int)a);
+        //     }
+        // }
+
+// upper one fails for zero
+
+        int n = nums.size();
+        long long prefix = 1;
+        long long suffix = 1;
         for (int i=0;i<n;i++)
         {
-            for(int j=i;j<n;j++)
-            {
-                int a;
-                if (i==0) a=P[j];
-                else if (P[i]!=0) a = P[j]/P[i-1];
-                else continue;
-            maxP = max(maxP,a);
-            }
+            if (prefix==0) prefix=1;
+            if(suffix ==0) suffix=1;
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[n-i-1];
+            maxP= max(maxP,(int)max(suffix,prefix));
         }
         return maxP;
+
 
     }
 };

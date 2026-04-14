7b8dec65b18d6082478465b60353e1e206198e06
charvi-web <charvisingh077@gmail.com>
2026-04-14 15:06:19 +0530

    Prefix sum ques

---
+++ PREFIXSUFFIX/runningsum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>res(n,0);
+        res[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            res[i]=res[i-1] + nums[i];
+        }
+        return res;
+
+    }
+};

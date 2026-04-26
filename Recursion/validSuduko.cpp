5ae7017ddfefbd276664160ba45fa76f892d0add
charvi-web <charvisingh077@gmail.com>
2026-04-27 02:22:51 +0530

    Suduko solver and validate sudoko

---
+++ Recursion/validSuduko.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+bool isValidOrNot(vector<vector<char>>&board,int row,int col,char c)
+{
+    for (int i=0;i<9;i++)
+    {
+        if (board[row][i]==c) return false;
+        if(board[i][col]==c) return false;
+        if (board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+    }
+    return true;
+}
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[0].size();j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    char c = board[i][j];
+                    board[i][j]='.';
+                    if (!isValidOrNot(board,i,j,c)) return false;
+                    board[i][j]=c;
+
+                }
+            }
+        }
+        return true;
+    }
+};
